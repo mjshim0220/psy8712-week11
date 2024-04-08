d1385c1130eefcb29d510acf787355d28107142a
MJ Shim <shim0220@umn.edu>
2024-04-07 20:03:29 -0500

    Completed Part 2

--- R/Week11.R
+++ R/Week11.R
@@ -5,6 +5,7 @@ library(haven)
 library(caret)
 library(parallel)
 library(doParallel)
+library(tictoc)
 
 #Data Import and Cleaning
 gss<-read_sav("../data/GSS2016.sav")
@@ -127,3 +128,82 @@ table1_tbl <- tibble(
 )
 
 table1_tbl
+
+#calculate the running time using function & tic and tock
+time <- function(model, training_tbl, na.pass) {
+  tic()  # Start the timer
+  model_fit <- train(`work hours` ~ ., 
+                     data = training_tbl, 
+                     method = model,
+                     trControl = trainControl(method="cv", 
+                                              number=10, 
+                                              verboseIter=T, 
+                                              indexOut = training_folds),
+                     preProcess = c("center","scale","zv","nzv","medianImpute"),
+                     na.action = na.pass)
+  time_taken <- toc(log = TRUE)  # End the timer and store the elapsed time
+  return(time_taken$toc - time_taken$tic)  # Return the elapsed time
+}
+
+# Corrected model method for linear regression with stepwise variable selection
+
+model_lm_time <- time("lm", training_tbl, na.pass)
+model_net_time <- time("glmnet", training_tbl, na.pass)
+model_rf_time <- time("rf", training_tbl, na.pass)
+model_xgb_time <- time("xgbLinear", training_tbl, na.pass)
+
+##create a table2_tbl
+# Set up parallel processing
+cl <- makeCluster(detectCores() - 1)
+registerDoParallel(cl)
+time_par <- function(model, training_tbl, na.pass) {
+  tic()  # Start the timer
+  model_fit <- train(`work hours` ~ ., 
+                     data = training_tbl, 
+                     method = model,
+                     trControl = trainControl(method="cv", 
+                                              number=10, 
+                                              verboseIter=T, 
+                                              indexOut = training_folds),
+                     preProcess = c("center","scale","zv","nzv","medianImpute"),
+                     na.action = na.pass)
+  time_taken <- toc(log = TRUE)  # End the timer and store the elapsed time
+  return(time_taken$toc - time_taken$tic)  # Return the elapsed time
+}
+
+model_lm_time_par <- time_par("lm", training_tbl, na.pass)
+model_net_time_par <- time_par("glmnet", training_tbl, na.pass)
+model_rf_time_par <- time_par("rf", training_tbl, na.pass)
+model_xgb_time_par <- time_par("xgbLinear", training_tbl, na.pass)
+
+#Stopparallization
+stopCluster(cl)
+registerDoSEQ()
+
+#create a table 2
+table2_tbl <- tibble(
+  algo = c("regression","elastic net","random forests","xgboost"),
+  original = c(
+    make_it_pretty(model_lm_time),
+    make_it_pretty(model_net_time),
+    make_it_pretty(model_rf_time),
+    make_it_pretty(model_xgb_time)
+  ),
+  parallelized = c(
+    make_it_pretty(model_lm_time_par),
+    make_it_pretty(model_net_time_par),
+    make_it_pretty(model_rf_time_par),
+    make_it_pretty(model_xgb_time_par)
+  )
+)
+
+table2_tbl
+
+#Q1. Which models benefited most from parallelization and why?
+## Based on the table random forest model presented the largest reduction in the running time when parallelized (220 --> 73.91 seconds).The random forest model's complex task works better under the paralleized condition.
+
+#Q2. How big was the difference between the fastest and slowest parallelized model? Why?
+###73.91 (Random forest model) - 2.23(Elastic net model). The gap between the running time is because of the complexity of the algorithm. 
+
+#Q3. If your supervisor asked you to pick a model for use in a production model, which would you recommend and why? Consider both Table 1 and Table 2 when providing an answer.
+##In terms of performance, the XGBoost model performs the best, followed by the random forests model. However, in terms of efficiency of model execution, the random forests model is the top performer, with XGBoost coming in next. Given that performance takes precedence over execution efficiency, I would recommend the random forests model to my supervisor.
