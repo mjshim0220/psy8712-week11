8fc8ebac7784f0861712ad4891379dce05aba529
MJ Shim <shim0220@umn.edu>
2024-04-09 12:55:15 -0500

    Final version

--- msi/week11.sh
+++ msi/week11.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=16
-#SBSTCH --mem=16gb
+#SBATCH --ntasks=10
+#SBSTCH --mem=60gb
 #SBATCH -t 01:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=shim0220@umn.edu
-#SBATCH -p amdsmall
+#SBATCH -p msilarge
 cd ~/psy8712-week11/msi
 module load R/4.3.0-openblas
 Rscript week11-cluster.R
